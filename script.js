9f102b3811fe6676accf9e9f4096532a66e1b701
Linkazen <jpch2k4@gmail.com>
2021-08-29 14:43:37 +0100

    nothing much added yet

---
+++ script.js
@@ -0,0 +1,15 @@
+function add(firNum, secNum) {
+    return firNum + secNum 
+}
+
+function subtract(firNum, secNum) {
+    return firNum - secNum
+}
+
+function multiply(firNum, secNum) {
+    return firNum * secNum
+}
+
+function Divide(firNum, secNum) {
+    return firNum / secNum
+}
